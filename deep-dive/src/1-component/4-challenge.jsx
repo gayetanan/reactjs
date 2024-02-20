6f5d1135306a166a4417e1c82e50d5d192910a96
gayetanan <gayetan.an@gmail.com>
2024-02-20 15:59:22 -0500

    component list

--- deep-dive/src/1-component/4-challenge.jsx
+++ deep-dive/src/1-component/4-challenge.jsx
@@ -1,18 +1,33 @@
 // create a page
-import img from './logo192.png'
-function FunPage(){
-    return (
-        <>
-            <img src={img} alt="react logo" />
-            <h1>Fun fact about react!</h1>
-            <ul>
-                <li>element 1</li>
-                <li>element 2</li>
-                <li>element 3</li>
-                <li>element 4</li>
-            </ul>     
-        </>
-    )
+import img from "./logo192.png";
+
+// turn list into component
+function MyListComponent() {
+  return (
+    <ul>
+      <li>element 1</li>
+      <li>element 2</li>
+      <li>element 3</li>
+      <li>element 4</li>
+    </ul>
+  );
 }
 
-export default FunPage
+function FunPage() {
+  return (
+    <>
+      <img src={img} alt="react logo" />
+      <h1>Fun fact about react!</h1>
+
+      {/* <ul>
+        <li>element 1</li>
+        <li>element 2</li>
+        <li>element 3</li>
+        <li>element 4</li>
+      </ul> */}
+      <MyListComponent />
+    </>
+  );
+}
+
+export default FunPage;
