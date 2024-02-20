d874edef1610ade392806635ccdc43eff4cad4a9
gayetanan <gayetan.an@gmail.com>
2024-02-20 15:05:00 -0500

    nested element

--- deep-dive/src/1-component/Jsx.jsx
+++ deep-dive/src/1-component/Jsx.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 // imparativelly create element
-const props = {className:"test-class"}
-const element = React.createElement("a",props,"i'm anchor");
+// const props = {className:"test-class"}
+// const element = React.createElement("a",props,"i'm anchor");
+// JSX
+// const jsx = <h1>Hello, from jsx</h1>;
+
+const JsxNestedElement =(
+  <React.Fragment>
+    <h1>hello nested element</h1>
+  </React.Fragment>
+)
 
 // render the element
 function RenderedElement(){
-  return element;
+  return  JsxNestedElement;
 }
-
 export  {RenderedElement};
