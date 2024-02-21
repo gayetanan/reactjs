7d47d742af39cc9c112d926ef64a8f06eab3a518
gayetanan <gayetan.an@gmail.com>
2024-02-21 02:15:45 -0500

    parent child component

--- deep-dive/src/1-component/4-challenge.jsx
+++ deep-dive/src/1-component/4-challenge.jsx
@@ -1,6 +1,17 @@
 // create a page
 import img from "./logo192.png";
 
+function Header(){
+  return <header>
+      <img src={img} alt="react logo" width="40px" />
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+  </header>
+}
 // turn list into component
 function MyListComponent() {
   return (
@@ -12,20 +23,23 @@ function MyListComponent() {
     </ul>
   );
 }
-
+function MainContent(){
+  return <>
+    <h1>Fun fact about react!</h1>
+      <MyListComponent />
+  </>
+}
+function Footer(){
+  return <footer>
+    <small>Hello footer</small>
+  </footer>
+}
 function FunPage() {
   return (
     <>
-      <img src={img} alt="react logo" />
-      <h1>Fun fact about react!</h1>
-
-      {/* <ul>
-        <li>element 1</li>
-        <li>element 2</li>
-        <li>element 3</li>
-        <li>element 4</li>
-      </ul> */}
-      <MyListComponent />
+    <Header/>
+    <MainContent/>
+    <Footer/>
     </>
   );
 }
