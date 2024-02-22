f529473bd3a8ea6fd58c7cf9808c1256bf2b8a8d
gayetanan <gayetan.an@gmail.com>
2024-02-22 15:09:38 -0500

    fun react project done

--- 4-info-site-challenge/src/components/Nav.jsx
+++ 4-info-site-challenge/src/components/Nav.jsx
@@ -1,3 +1,14 @@
+import logo from "../assets/logo192.png"
 export default function Nav(){
-    return <h1>Nav bar goes here</h1>
+    return <nav>
+       <div className="container">
+            <div className="content">
+                <div className="logo">
+                    <img src={logo} alt="react logo" />
+                    <h3>ReactFacts</h3>
+                </div>
+                <h4>React Course - Project 1</h4>
+            </div>
+       </div>
+    </nav>
 }
