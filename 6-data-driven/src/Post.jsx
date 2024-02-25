84dbabcf838f90f6baab8af07616e66b25592d55
gayetanan <gayetan.an@gmail.com>
2024-02-25 17:53:27 -0500

    use other js datatype

--- 6-data-driven/src/Post.jsx
+++ 6-data-driven/src/Post.jsx
@@ -1,11 +1,19 @@
 // conditional rendering
-export default function Post({ title, desc }) {
+export default function Post({ title, body, isPublic, comments }) {
   const postTitle = title ? <h1>{title}</h1> : null;
   return (
-    <div>
+    <div style={{ opacity: isPublic ? 1 : 0.5 }}>
       {postTitle}
       {/* short circuit */}
-      {desc && <p>{desc}</p>}
+      {body && <p>{body}</p>}
+      {comments && (
+        <ul>
+          <li>
+            <h3>{comments[0].user}</h3>
+            <p>{comments[0].body}</p>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
--- 6-data-driven/src/Posts.jsx
+++ 6-data-driven/src/Posts.jsx
@@ -3,7 +3,20 @@ import Post from "./Post";
 export default function Posts() {
   return (
     <div>
-      <Post titl="I'm a post" desc="Hello world !" />
+      <Post
+        title="I'm a post"
+        body="Hello world !"
+        isPublic={false}
+        upVotes={3000}
+        comments={[{ user: "gayetan.an", body: "ok" }]}
+      />
+      <Post
+        title="I'm a post"
+        body="Hello world !"
+        isPublic={true}
+        upVotes={3000}
+        // comments={[{ user: "gayetan.an", body: "ok" }]}
+      />
     </div>
   );
 }
