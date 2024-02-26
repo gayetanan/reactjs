2432a57c37a9778662ab960b9e4267735e98a331
gayetanan <gayetan.an@gmail.com>
2024-02-26 07:07:58 -0500

    generate JSX with data list

--- 6-data-driven/src/Post.jsx
+++ 6-data-driven/src/Post.jsx
@@ -7,11 +7,21 @@ export default function Post({ title, body, isPublic, comments }) {
       {/* short circuit */}
       {body && <p>{body}</p>}
       {comments && (
+        // <ul>
+        //   <li>
+        //     <h3>{comments[0].user}</h3>
+        //     <p>{comments[0].body}</p>
+        //   </li>
+        // </ul>
         <ul>
-          <li>
-            <h3>{comments[0].user}</h3>
-            <p>{comments[0].body}</p>
-          </li>
+          {comments.map((comment) => {
+            return (
+              <li>
+                <h3>{comment.user}</h3>
+                <p>{comment.body}</p>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
--- 6-data-driven/src/Posts.jsx
+++ 6-data-driven/src/Posts.jsx
@@ -8,14 +8,17 @@ export default function Posts() {
         body="Hello world !"
         isPublic={false}
         upVotes={3000}
-        comments={[{ user: "gayetan.an", body: "ok" }]}
+        comments={[
+          { user: "gayetan.an", body: "ok mama" },
+          { user: "mamapina", body: "ok" },
+        ]}
       />
       <Post
         title="I'm a post"
         body="Hello world !"
         isPublic={true}
         upVotes={3000}
-        // comments={[{ user: "gayetan.an", body: "ok" }]}
+        comments={[{ user: "gayetan.an", body: "ok" }]}
       />
     </div>
   );
